e69752b15e31f1d669cc62ef39b56aec054d1e7f
Thabisomat <korowethabiso@gmail.com>
2024-10-31 01:52:32 +0200

    Created a functions to click Customer login button

---
+++ pages/customerLoginPage.py
@@ -0,0 +1,15 @@
+from lib2to3.pgen2 import driver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class CustomerLoginPage:
+    button_CustomerLogins_xpath = "//button[contains(.,'Customer Login')]"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def clickCustomerLoginsButton(self):
+        self.driver.find_element(By.XPATH, self.button_CustomerLogins_xpath).click()

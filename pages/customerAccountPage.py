20de8dbe4b85c8cceaee45bbe70c0a957890fc33
Thabisomat <korowethabiso@gmail.com>
2024-10-31 12:13:46 +0200

    Created a functions to deposit money into account

--- pages/customerAccountPage.py
+++ pages/customerAccountPage.py
@@ -15,11 +15,12 @@ class CustomerAccountPage:
         accounts = self.driver.find_elements(By.ID, "accountSelect")
         accounts[account_index + 1].click()
 
- def deposit(self, amount):
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Deposit')]"))
-        ).click()
 
-        deposit_input = self.driver.find_element(By.XPATH, "//input[@ng-model='amount']")
-        deposit_input.send_keys(amount)
-        self.driver.find_element(By.XPATH, "//button[text()='Deposit']").click()
+def deposit(self, amount):
+    WebDriverWait(self.driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Deposit')]"))
+    ).click()
+
+    deposit_input = self.driver.find_element(By.XPATH, "//input[@ng-model='amount']")
+    deposit_input.send_keys(amount)
+    self.driver.find_element(By.XPATH, "//button[text()='Deposit']").click()
